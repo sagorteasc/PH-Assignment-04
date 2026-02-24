ef349543372caba96ffedeedde6b8a93eaff9d6c
Md Naimul Islam Sagor <islamsagor49368@gmail.com>
2026-02-24 15:26:47 +0600

    fixed button functionality

--- js/all.js
+++ js/all.js
@@ -2,7 +2,7 @@ const allCards = document.getElementById('cards');
 document.getElementById('all-section').addEventListener('click', function () {
     document.getElementById('interview-section').classList.remove('bg-[#3B82F6]', 'text-white');
     document.getElementById('rejected-section').classList.remove('bg-[#3B82F6]', 'text-white');
-    this.className = 'btn bg-[#3B82F6] font-medium text-white px-3 py-2 rounded';
+    this.className = 'btn bg-[#3B82F6] font-medium text-white px-8 py-[10px] rounded';
     allCards.classList.remove('hidden');
 
     const cards = document.querySelectorAll('#cards .card');
